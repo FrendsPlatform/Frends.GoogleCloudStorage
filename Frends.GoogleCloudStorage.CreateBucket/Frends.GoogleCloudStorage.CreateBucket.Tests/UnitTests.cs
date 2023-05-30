3af3cb623136392fd849851950a2e1e43229a070
Riku Virtanen <virtanen.riku91@gmail.com>
2023-05-30 12:45:41 +0300

    Modified test to use environment variable in base64 format

--- Frends.GoogleCloudStorage.CreateBucket/Frends.GoogleCloudStorage.CreateBucket.Tests/UnitTests.cs
+++ Frends.GoogleCloudStorage.CreateBucket/Frends.GoogleCloudStorage.CreateBucket.Tests/UnitTests.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using System;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text;
 using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Storage.V1;
 using Frends.GoogleCloudStorage.CreateBucket.Definitions;
@@ -12,9 +13,9 @@ namespace Frends.GoogleCloudStorage.CreateBucket.Tests;
 [TestFixture]
 class UnitTests
 {
-    //private readonly string _credentialsJson = Environment.GetEnvironmentVariable("Frends_GoogleCloudStorage_CredJson");
+    private readonly string _credentialsBase64 = Environment.GetEnvironmentVariable("Frends_GoogleCloudStorage_CredJson");
     private readonly string _projectId = "instant-stone-387712";
-    private readonly string _bucketName = "test-bucket";
+    private readonly string _bucketName = "test-bucket9bc24fe0-77a0-4fb2-9b15-5e6f66972c6e";
     private readonly string _location = "US-CENTRAL1";
     private readonly StorageClassType _storageClass = StorageClassType.STANDARD;
     private readonly string _path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../credentials.json");
@@ -23,7 +24,8 @@ class UnitTests
     [SetUp]
     public void Setup()
     {
-        //File.WriteAllText(_path, _credentialsJson);
+        var base64EncodedBytes = Convert.FromBase64String(_credentialsBase64);
+        File.WriteAllText(_path, Encoding.ASCII.GetString(base64EncodedBytes));
         _input = new Input()
         {
             BucketName = _bucketName,
@@ -32,19 +34,18 @@ class UnitTests
             ProjectId = _projectId,
             CredentialFilePath = _path,
             CredentialJson = "",
-            AddGuidToBucketName = true
+            AddGuidToBucketName = false
         };
     }
 
     [TearDown]
     public async Task Teardown()
     {
-        //File.Delete(_path);
-
         using var client = await StorageClient.CreateAsync(GoogleCredential.FromFile(_path));
         var buckets = client.ListBuckets(_projectId);
         foreach (var bucket in buckets)
             client.DeleteBucket(bucket.Name);
+        File.Delete(_path);
     }
 
     [Test]
@@ -52,8 +53,7 @@ class UnitTests
     {
         var result = await GoogleCloudStorage.CreateBucket(_input, default);
         Assert.IsNotNull(result);
-        var name = result.BucketName;
-        Assert.AreEqual(name, name);
+        Assert.AreEqual(_bucketName, result.BucketName);
     } 
 }
 
